8e57813ba58498ce1a878efe799a58a9822a6f21
wangjiankun <wangjiankun_moreji@163.com>
2022-08-12 15:54:23 +0800

    feat: 示例

--- src/App.vue
+++ src/App.vue
@@ -1,113 +1,132 @@
 <template>
   <div id="app">
-   <BasicForm @register="register"></BasicForm>
+    <div style="padding: 0 36px; margin: 56px">
+      <BasicForm @register="register"></BasicForm>
+    </div>
   </div>
 </template>
 
 <script>
-import { BasicForm, useForm } from './components/BasicForm'
+import { BasicForm, useForm } from "./components/BasicForm";
+// const rowProps = { span: 24, lg: 24, sm: 24, xl: 24, xxl: 24 };
+// const halfProps = { md: 24, lg: 12 };
+const thirdProps = { lg: 8, xs: 24, sm: 24, xl: 8, xxl: 8, span: 8 };
+
 const [register, methods] = useForm({
-  'label-position': 'right',
-  'hide-required-asterisk': false,
-  'label-suffix': '：',
-  'label-width': 'auto',
-  size: 'mini',
+  "label-position": "right",
+  "hide-required-asterisk": false,
+  "label-suffix": "：",
+  "label-width": "auto",
+  rowProps: { gutter: "20" },
+  size: "mini",
   fields: [
-    {  
-      prop: 'name',
-      label: '姓名', 
-      component: 'Input',
+    {
+      prop: "name",
+      label: "姓名",
+      component: "Input",
       componentProps: {
-        placeholder: '请输入姓名',
-        onChange({ value, action, model }){
-          console.log(value, 'on-change');
-          console.log(action, 'on-change-action');
-          console.log(model, 'on-change-model')
-        }
+        placeholder: "请输入姓名",
+        onChange({ value, action, model }) {
+          console.log(value, "on-change");
+          console.log(action, "on-change-action");
+          console.log(model, "on-change-model");
+        },
       },
-      defaultValue: '青年', 
-      colProps: { span: 12 }, 
-      // ifShow: ({ model }) => {
-      //   return model.age >= 18
-      // }, 
+      defaultValue: "",
+      colProps: thirdProps,
+      ifShow: ({ model }) => {
+        return model.age >= 20;
+      },
       show: () => true,
-      rules: [{ required: true, message: '请输姓名', trigger: 'blur' }],
+      rules: [{ required: true, message: "请输姓名", trigger: "blur" }],
       error: undefined,
       required: false,
-      ['inline-message']: false,
-      ['show-message']: true,
+      ["inline-message"]: false,
+      ["show-message"]: true,
     },
-    {  
-      prop: 'age',
-      label: '年龄段', 
-      component: 'Slider',
+    {
+      prop: "age",
+      label: "年龄段",
+      component: "Select",
       componentProps: {
         options: [
-          { label: '17', value: 17 },
-          { label: '18', value: 18 }
+          { label: '15', value: 15 },
+          { label: '20', value: 20 },
+          { label: "30", value: 30 },
+          { label: "40", value: 40 },
+          { label: "50", value: 50 },
+          { label: "60", value: 60 },
+          { label: "70", value: 70 },
+          { label: "80", value: 80 },
         ],
-        placeholder: '请选择年龄',
-        onChange({ value, action }){
-          const { setFieldsValue } = action
-          let age = ''
-          if(value <= 10 ) {
-            age = '少年' 
-          } else if(value <= 17) {
-            age = '青少年'
-          } else if(value <= 24) {
-            age = '青年'
-          } else if(value <= 35) {
-            age = '而立'
-          } else if(value <= 45) {
-            age = '不惑'
-          } else if(value <= 55) {
-            age = '知天命'
-          } else if(value <= 65) {
-            age = '耳顺'
-          } else if(value <= 75) {
-            age = '花甲'
-          } else if(value <= 85) {
-            return '古稀'
+        placeholder: "请选择年龄",
+        onChange({ value, action }) {
+          const { setFieldsValue } = action;
+          let name = ''
+          switch(value) {
+            case 15: 
+              name = '至于学'
+              break;
+            case 20: 
+              name = '少年' 
+              break;
+            case 30:
+              name = '而立'
+              break;
+            case 40: 
+              name = '不惑'
+              break;
+            case 50: 
+              name = '知天命'
+              break; 
+            case 60: 
+              name = '耳顺'
+              break;
+            case 70:
+              name = '花甲'
+              break;
+            case 80:
+              name = '古稀'
+              break;
+            default: break;
           }
-          setFieldsValue({ name: age });
-        }
+          setFieldsValue({ name });
+        },
       },
-      defaultValue: 17,
-      colProps: { span: 12 }, 
-      rules: [{ required: true, message: '请选择年龄', trigger: 'blur' }],
+      defaultValue: '',
+      colProps: thirdProps,
+      rules: [{ required: true, message: "请选择年龄", trigger: "blur" }],
       error: undefined,
       required: false,
-      ['inline-message']: false,
-      ['show-message']: true,
-
-    }
-  ]
-})
+      ["inline-message"]: false,
+      ["show-message"]: true,
+    },
+  ],
+});
 export default {
-  name: 'App',
+  name: "App",
   data() {
     return {
       register,
       methods,
-    }
-  },  
+    };
+  },
   components: {
     BasicForm,
   },
   methods: {
     inputChange() {
-      console.log(arguments)
-    }
+      console.log(arguments);
+    },
   },
   async mounted() {
+    const values = this.methods.getFieldsValue();
+    console.log(values);
 
-   const values = this.methods.getFieldsValue();
-   console.log(values);
-
-   setTimeout( async () => {
-     const res = await this.methods.validate();
-     console.log(res);
-   }, 2000)
-  }
-}
+    setTimeout(async () => {
+      // const res = await this.methods.validate();
+      // console.log(res);
+    }, 2000);
+  },
+};
 </script>
